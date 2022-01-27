4b891d0c44be8e320af0a156ca358f6265a048f1
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-01-26 18:22:43 -0800

    Initial Commit

---
+++ amateur/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  amateur
+//
+//  Created by Grace on 1/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ amateur/amateurApp.swift
@@ -0,0 +1,17 @@
+//
+//  amateurApp.swift
+//  amateur
+//
+//  Created by Grace on 1/26/22.
+//
+
+import SwiftUI
+
+@main
+struct amateurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
