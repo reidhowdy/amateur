1c2a43c54b1df775bf8357e26b7f40b9e4a1b74f
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-01-27 17:44:03 -0800

    Wrote addOffer() and getOffers() functions in OfferViewModel.

--- amateur/ViewModels/OfferViewModel.swift
+++ amateur/ViewModels/OfferViewModel.swift
@@ -2,12 +2,78 @@ import Foundation
 import Firebase
 
 class OfferViewModel: ObservableObject {
-    @Published var list = [Offer]()
+    @Published var offerList = [Offer]()
     
+    
+    //add an instance of Offer to the db
     func addOffer(id: String, title: String, typeOfOffer: String, estimatedTime: Float, numSessions: Int, datePosted: Date, materialsNeeded: String, description: String, locationPreferences: String, onlineOnly: Bool, username: String) {
         
         let db = Firestore.firestore()
         
-        db.collection("")
+        db.collection("amateur-capstone")
+            .addDocument(data: [
+                "id": id,
+                "title": title,
+                "typeOfOffer": typeOfOffer,
+                "estimatedTime": estimatedTime,
+                "numSessions": numSessions,
+                "datePosted": datePosted,
+                "materialsNeeded": materialsNeeded,
+                "description": description,
+                "locationPreferences": locationPreferences,
+                
+            ]) { error in
+                if error == nil {
+                    self.getOffers()
+                }
+                else {
+                    print("Oops. There was an erorr.")
+                }
+            }
     }
+    
+    
+    //get data from one instance from the db
+    //make it into an instance of Offer
+    func getOffers() {
+        let db = Firestore.firestore() //ref to our db
+        
+        db.collection("amateur-capstone").getDocuments() {
+            snapshot, error in
+            
+            if error == nil {
+                
+                if let snapshot = snapshot {
+                    DispatchQueue.main.async {
+                        self.offerList = snapshot.documents.map { doc in
+                            return Offer(
+                                id: doc.documentID,
+                                title: doc["title"]                             as? String ?? "None",
+                                typeOfOffer: doc["typeOfOffer"]                 as? String ?? "None",
+                                estimatedTime: doc["estimatedTime"]             as? Float ?? 0.0,
+                                numSessions: doc["numSessions"]                 as? Int ?? 0,
+                                datePosted: doc["datePosted"]                   as? Date ?? Date.now, //this default isn't right
+                                materialsNeeded: doc["materialsNeeded"]         as? String ?? "None",
+                                description: doc["description"]                 as? String ?? "None",
+                                locationPreferences: doc["locationPreferences"] as? String ?? "None",
+                                onlineOnly: doc["onlineOnly"]                   as? Bool ?? false,
+                                username: doc["username"]                       as? String ?? "None"
+                                )
+                        }
+                    }//DQ bracket
+                    
+                    print("Test")
+                    print(self.offerList)
+                    print("End test")
+                    
+                }
+            }
+            else { //else, if there is an error
+                print("Oops. There was an error.")
+            }
+        }
+    }
+    
+    
+    
 }
