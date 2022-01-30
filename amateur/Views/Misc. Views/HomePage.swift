1a468cba215ee14f73372904fdf81b4b4ebaa82c
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-01-30 11:14:07 -0800

    Rearranged home page so that the success ScrollView is within the existing NavView. Fixes earlier UI bug.

--- amateur/Views/Misc. Views/HomePage.swift
+++ amateur/Views/Misc. Views/HomePage.swift
@@ -4,32 +4,35 @@ struct HomePage: View {
     var body: some View {
         VStack {
             NavigationView {
-                List { //probably change this to a VStack
-                    NavigationLink("Browse Available Offers",
-                        destination: OfferFeed())
-                    NavigationLink("Browse Current Asks",
-                        destination: AskFeed())
-                    NavigationLink("Post an Offer",
-                        destination: OfferPost())
-                    NavigationLink("Post an Ask",
-                        destination: AskPost())
-                }
-                .navigationTitle("Amateur")
-            }
-            NavigationView {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 20) {
-                        ForEach(0..<10) {
-                            NavigationLink("Success \($0)",
-                                           destination: SuccessDetail())
-                                .foregroundColor(.white)
-                                .font(.largeTitle)
-                                .frame(width: 200, height: 200)
-                                .background(Color.yellow)
+                VStack{
+                    List { //probably change this to a VStack
+                        NavigationLink("Browse Available Offers",
+                            destination: OfferFeed())
+                        NavigationLink("Browse Current Asks",
+                            destination: AskFeed())
+                        NavigationLink("Post an Offer",
+                            destination: OfferPost())
+                        NavigationLink("Post an Ask",
+                            destination: AskPost())
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            ForEach(0..<10) {
+                                NavigationLink("Success \($0)",
+                                               destination: SuccessDetail())
+                                    .foregroundColor(.white)
+                                    .font(.largeTitle)
+                                    .frame(width: 200, height: 200)
+                                    .background(Color.yellow)
+                            }
                         }
                     }
-                }
-                .navigationTitle("Success Stories")
+                    .navigationTitle("Amateur")
+            
+            }
+                //NavigationView {
+                
+                //.navigationTitle("Success Stories")
             }
         }
     }
--- amateur/Views/Success Views/SuccessDetail.swift
+++ amateur/Views/Success Views/SuccessDetail.swift
@@ -1,15 +1,8 @@
-//
-//  SuccessDetail.swift
-//  amateur
-//
-//  Created by Grace on 1/30/22.
-//
-
 import SwiftUI
 
 struct SuccessDetail: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("This is SuccessDetail")
     }
 }
 
