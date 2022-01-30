1420c5d8b02b43b0ec398393144f21b550c9a0e1
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-01-30 12:13:08 -0800

    Created AskPost form, which takes in info in a Form and then posts it to the asks collection in the db. Need to work out the issue with estimatedTime-- there seems to be an issue with type casting between string and float

--- amateur/Views/Ask Views/AskPost.swift
+++ amateur/Views/Ask Views/AskPost.swift
@@ -1,15 +1,58 @@
-//
-//  AskPost.swift
-//  amateur
-//
-//  Created by Grace on 1/29/22.
-//
-
 import SwiftUI
 
+//the general workflow:
+//create state variable for the piece of data
+
+//create a nav view
+    //embed a Form within it
+    //embed a TextField within that
+
+
+//'typeOfAsk', 'estimatedTime', 'datePosted', 'description', 'materialsNeeded', 'locationPreferences', 'onlineOnly', 'username' in call
 struct AskPost: View {
+    @ObservedObject var askViewModel = AskViewModel()
+    
+    @State var title: String = ""
+    @State var typeOfAsk: String = ""
+    @State var estimatedTime: Float = 0.0
+    //do not need datePosted state, it's always gonna be Date.now
+    @State var description: String = ""
+    @State var materialsNeeded: String = ""
+    @State var locationPreferences: String = ""
+    @State var onlineOnly: Bool = true
+    @State var username: String = ""
+    
+    
     var body: some View {
-        Text("Post an ask here!")
+        NavigationView {
+            Form {
+                TextField("Title",
+                          text: $title)
+                TextField("Type of ask",
+                          text: $typeOfAsk)
+//                TextField("Est. time",
+//                          text: Float($estimatedTime))
+                TextField("Description",
+                          text: $description)
+                TextField("Materials needed",
+                          text: $materialsNeeded)
+                TextField("Location preferences",
+                          text: $locationPreferences)
+                Toggle(isOn: $onlineOnly) {
+                    Text("Online only")
+                }
+                TextField("Username",
+                          text: $username)
+                
+                
+                
+                Button("Post", action: {
+                    (askViewModel.addAsk(title: title, typeOfAsk: typeOfAsk, estimatedTime: estimatedTime, datePosted: Date.now, description: description, materialsNeeded: materialsNeeded, locationPreferences: locationPreferences, onlineOnly: onlineOnly, username: username))
+                })
+            }
+            .navigationTitle("Post Your Ask")
+        }
+        
     }
 }
 
--- amateur/Views/Misc. Views/HomePage.swift
+++ amateur/Views/Misc. Views/HomePage.swift
@@ -2,37 +2,34 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        VStack {
-            NavigationView {
-                VStack{
-                    List { //probably change this to a VStack
-                        NavigationLink("Browse Available Offers",
-                            destination: OfferFeed())
-                        NavigationLink("Browse Current Asks",
-                            destination: AskFeed())
-                        NavigationLink("Post an Offer",
-                            destination: OfferPost())
-                        NavigationLink("Post an Ask",
-                            destination: AskPost())
-                    }
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            ForEach(0..<10) {
-                                NavigationLink("Success \($0)",
-                                               destination: SuccessDetail())
-                                    .foregroundColor(.white)
-                                    .font(.largeTitle)
-                                    .frame(width: 200, height: 200)
-                                    .background(Color.yellow)
-                            }
+        NavigationView {
+            VStack{
+                List { //probably change this to a VStack
+                    NavigationLink("Browse Available Offers",
+                        destination: OfferFeed())
+                    NavigationLink("Browse Current Asks",
+                        destination: AskFeed())
+                    NavigationLink("Post an Offer",
+                        destination: OfferPost())
+                    NavigationLink("Post an Ask",
+                        destination: AskPost())
+                }
+                Text("Browse Success Stories")
+                    .font(.title)
+                    
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<10) {
+                            NavigationLink("Success \($0)",
+                                           destination: SuccessDetail())
+                                .foregroundColor(.white)
+                                .font(.largeTitle)
+                                .frame(width: 200, height: 300)
+                                .background(Color.yellow)
                         }
                     }
-                    .navigationTitle("Amateur")
-            
-            }
-                //NavigationView {
-                
-                //.navigationTitle("Success Stories")
+                }
+                .navigationTitle("Amateur")
             }
         }
     }
