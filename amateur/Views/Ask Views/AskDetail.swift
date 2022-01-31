79cc379cfeda3b532e6921393a99a4581f7c3212
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-01-31 07:24:21 -0800

    Fixed some display issues within AskDetail.

--- amateur/Views/Ask Views/AskDetail.swift
+++ amateur/Views/Ask Views/AskDetail.swift
@@ -7,25 +7,23 @@ struct AskDetail: View {
         VStack {
             Spacer()
             HStack {
-                Text("ask title")
+                Text(ask.title)
                     .font(.title)
                     .multilineTextAlignment(.leading)
                     .padding([.top, .leading, .bottom])
                 Spacer()
-                Text("ask description")
+                Text(ask.description)
                     .font(.body)
                     .multilineTextAlignment(.trailing)
                     .padding(.all)
             }
             Spacer()
             Text("figure out how to format the date here")
-            Text("Est time to complete: *ask time here* hours")
-                .multilineTextAlignment(.leading)
-            Text("Number of Est. Sessions: *number sessions here*")
+            Text("Est time to complete: \(ask.estimatedTime) hours")
                 .multilineTextAlignment(.leading)
             Spacer()
         }
-        .navigationTitle("Offer Details")
+        .navigationTitle("Ask Details")
         .edgesIgnoringSafeArea(.all) //can change .all to things like .bottom, etc.
     }
 }
