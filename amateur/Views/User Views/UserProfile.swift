b7c480b82b438c4e795ef3fa585bd8a28bd2f252
reidhowdy <87451286+reidhowdy@users.noreply.github.com>
2022-02-01 11:07:26 -0800

    Deleted plist.

---
+++ amateur/Views/User Views/UserProfile.swift
@@ -0,0 +1,20 @@
+//
+//  UserProfile.swift
+//  amateur
+//
+//  Created by Grace on 2/1/22.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
